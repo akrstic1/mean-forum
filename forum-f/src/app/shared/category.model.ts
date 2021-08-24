01721c196a649e1d3778a5e669fa5aca4726e8af
akrstic1 <arno.krstic@gmail.com>
2021-08-24 17:46:42 +0200

    bug fixes, strict templates

--- forum-f/src/app/shared/category.model.ts
+++ forum-f/src/app/shared/category.model.ts
@@ -1,8 +1,8 @@
 export class Category {
-    public constructor(id : string, name : string){
-        this.id = id;
-        this.name = name;
-    }
-    id : string;
-    name : string;
+  public constructor(id: string, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+  id: string;
+  name: string;
 }
--- forum-f/src/app/shared/reply.model.ts
+++ forum-f/src/app/shared/reply.model.ts
@@ -1,5 +1,5 @@
-export class Reply{
-    reply_text : string;
-    reply_user_id : string;
-    reply_date : Date;
+export class Reply {
+  reply_text: string;
+  reply_user_id: string;
+  reply_date: Date;
 }
